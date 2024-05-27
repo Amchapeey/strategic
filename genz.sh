00811a3fe56d36c87956b06ca61c2f5532392c98
Amchapeey <ir612987@gmail.com>
2024-05-28 00:47:22 +0300

    genz.sh

--- genz.sh
+++ genz.sh
@@ -310,8 +310,8 @@ clear
 clear
 #CHANGE PASSWORD DEFAULT
 restart_system(){
-    USRSC=$(curl -sS https://raw.githubusercontent.com/kankaungmaung/izin/main/ip | grep $MYIP | awk '{print $2}')
-    EXPSC=$(curl -sS https://raw.githubusercontent.com/kankaungmaung/izin/main/ip | grep $MYIP | awk '{print $3}')
+    USRSC=$(curl -sS https://raw.githubusercontent.com/Amchapeey/keygen/main/ip | grep $MYIP | awk '{print $2}')
+    EXPSC=$(curl -sS https://raw.githubusercontent.com/Amchapeey/keygen/main/ip | grep $MYIP | awk '{print $3}')
     TIMEZONE=$(printf '%(%H:%M:%S)T')
     TEXT="
 <code>────────────────────</code>
