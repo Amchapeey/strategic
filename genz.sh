49d186bce970cb105ed24f7f42c23f4035ca1c78
Amchapeey <ir612987@gmail.com>
2024-05-28 00:52:21 +0300

    genz.sh

--- genz.sh
+++ genz.sh
@@ -96,9 +96,9 @@ clear
 #########################
 # USERNAME
 rm -f /usr/bin/user
-username=$(curl https://raw.githubusercontent.com/Amchapeey/strategic/main/ | grep $MYIP | awk '{print $2}')
+username=$(curl https://raw.githubusercontent.com/Amchapeey/strategic/main/keygen | grep $MYIP | awk '{print $2}')
 echo "$username" >/usr/bin/user
-expx=$(curl https://raw.githubusercontent.com/Amchapeey/strategic/main/ | grep $MYIP | awk '{print $3}')
+expx=$(curl https://raw.githubusercontent.com/Amchapeey/strategic/main/keygen | grep $MYIP | awk '{print $3}')
 echo "$expx" >/usr/bin/e
 # DETAIL ORDER
 username=$(cat /usr/bin/user)
@@ -122,7 +122,7 @@ mai="datediff "$Exp" "$DATE""
 Info="(${green}Active${NC})"
 Error="(${RED}ExpiRED${NC})"
 today=`date -d "0 days" +"%Y-%m-%d"`
-Exp1=$(curl https://raw.githubusercontent.com/kankaungmaung/shadow/main/izin | grep $MYIP | awk '{print $4}')
+Exp1=$(curl https://raw.githubusercontent.com/Amchapeey/strategic/main/keygen | grep $MYIP | awk '{print $4}')
 if [[ $today < $Exp1 ]]; then
 sts="${Info}"
 else
