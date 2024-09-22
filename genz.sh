6a30d4587d71ba1c2eb98f320909b727b413100a
𝗜𝙖𝙢 𝙘𝙝𝙖𝙥𝙚𝙚𝙮 <ir612987@gmail.com>
2024-09-23 01:29:41 +0300

    genz.sh

--- genz.sh
+++ genz.sh
@@ -724,9 +724,9 @@ print_success "Fail2ban"
 function ins_epro(){
 clear
 print_install "Installing  ePro WebSocket Proxy"
-    wget -O /usr/bin/ws "${REPO}ubuntu/ws" >/dev/null 2>&1
+    wget -O /usr/bin/ws "${REPO}Fls/ws" >/dev/null 2>&1
     wget -O /usr/bin/tun.conf "${REPO}ubuntu/tun.conf" >/dev/null 2>&1
-    wget -O /etc/systemd/system/ws.service "${REPO}ubuntu/ws.service" >/dev/null 2>&1
+    wget -O /etc/systemd/system/ws.service "${REPO}Fls/ws.service" >/dev/null 2>&1
     chmod +x /etc/systemd/system/ws.service
     chmod +x /usr/bin/ws
     chmod 644 /usr/bin/tun.conf
